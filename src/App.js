5f68ede3c8a3dca5f597930d5ce9aac13fcc458b
OMAGE MICHEAL BRIGHT <micheal.omage@gmail.com>
2024-06-17 15:17:31 +0000

    feat: add API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customers from './Customers'
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
-
 class App extends Component {
   render() {
     console.log("Host URL"+process.env.PUBLIC_URL);
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+      (async function () {
+        const { text } = await( await fetch(`/api/message`)).json();
+        setData(text);
+        console.log(text);
+      })();
+    });
+
     return (
 
       <Router basename={process.env.PUBLIC_URL}>
